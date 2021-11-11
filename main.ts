ee6d38ec96168cd307112f9a7739506106d786c5
leejinwoo4936 <94099563+leejinwoo4936@users.noreply.github.com>
2021-11-11 16:31:14 +0900

    조명 센서 컨트롤

--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-    soundExpression.giggle.playUntilDone()
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Square)
-    soundExpression.happy.playUntilDone()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No)
-    soundExpression.hello.playUntilDone()
-})
 basic.forever(function () {
-	
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
