b2b36feaf887478a934c4eb4c947e301f1289108
leejinwoo4936 <94099563+leejinwoo4936@users.noreply.github.com>
2021-11-11 16:11:10 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.giggle.playUntilDone()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Square)
+    soundExpression.happy.playUntilDone()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    soundExpression.hello.playUntilDone()
+})
 basic.forever(function () {
 	
 })
